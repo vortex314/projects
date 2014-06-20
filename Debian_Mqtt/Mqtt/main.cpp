55bdaa56ffeefdee0c49f202a0ca565217c0d11d
Lieven Merckx <lieven.merckx@gmail.com>
2014-06-21 01:35:30 +0200

    tcp socket

--- Debian_Mqtt/Mqtt/main.cpp
+++ Debian_Mqtt/Mqtt/main.cpp
@@ -1,17 +1,120 @@
 #include <iostream>
 #include "Bytes.h"
 #include "Str.h"
+#include "MqttIn.h"
+#include "MqttOut.h"
 
-using namespace std;
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
 
-
-
-int main()
+class Tcp
 {
-    cout << "Hello world!" << endl;
-    Bytes bytes(10);
-Str str(200);
-    str.append("Hello world ");
-    cout << str.data();
-    return 0;
+public :
+    Tcp();
+    Erc connect(char *ip,int port);
+    Erc disconnect();
+    Erc read(uint8_t *pb,uint32_t *length);
+    Erc send(Bytes* pb);
+    Erc recv(Bytes* pb);
+private :
+    int _sockfd;
+
+};
+
+Tcp::Tcp()
+{
 }
+
+Erc Tcp::connect(char *ip,int portno)
+{
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    /* Create a socket point */
+    _sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (_sockfd < 0)
+    {
+        return E_INVAL;
+    }
+    server = gethostbyname(ip);
+    if (server == NULL)
+    {
+        return E_NOT_FOUND;
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    /* Now connect to the server */
+    if (::connect(_sockfd,(const sockaddr*)&serv_addr,sizeof(serv_addr)) < 0)
+    {
+        return E_CONN_LOSS;
+    }
+    return E_OK;
+}
+
+Erc Tcp::disconnect()
+{
+    close(_sockfd);
+    return E_OK;
+}
+
+
+Erc Tcp::read(uint8_t *pb,uint32_t *length)
+{
+    int n;
+    n=::read(_sockfd,pb,*length) ;
+    if (n < 0)
+    {
+        return E_CONN_LOSS;
+    }
+    return E_OK;
+}
+
+Erc Tcp::send(Bytes* pb)
+{
+    int n;
+    n=write(_sockfd,pb->data(),pb->length()) ;
+    if (n < 0)
+    {
+        return E_CONN_LOSS;
+    }
+    return E_OK;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    Tcp tcp;
+    MqttOut mqttOut(256);
+    MqttIn mqttIn(256);
+    Str str(100);
+    uint16_t messageId=1000;
+
+    tcp.connect("test.mosquitto.org",1883);
+    mqttOut.Connect(0, "clientId", MQTT_CLEAN_SESSION,
+                    "ikke/alive", "false", "userName", "password", 1000);
+    tcp.send(&mqttOut);
+
+    Str topic(100),msg(100);
+    topic.append("ikke/topic");
+    msg.append("value");
+    mqttOut.Publish(MQTT_QOS2_FLAG, &str, &msg, messageId);
+
+    tcp.send(&mqttOut);
+    mqttOut.PubRel(messageId);
+    tcp.send(&mqttOut);
+    mqttOut.Disconnect();
+    tcp.send(&mqttOut);
+    tcp.disconnect();
+
+}
+
