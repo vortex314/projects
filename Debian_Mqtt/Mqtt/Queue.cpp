dda0f011dd447a36bc7de934d2072b7c01ead46d
Lieven Merckx <lieven.merckx@gmail.com>
2014-06-22 15:24:37 +0200

    added Queue

---
+++ Debian_Mqtt/Mqtt/Queue.cpp
@@ -0,0 +1,42 @@
+#include "Queue.h"
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>    /* For mode constants */
+#include <mqueue.h>
+#include "Sys.h"
+#include <stdio.h>
+#include <errno.h>
+
+#define PQ ((mqd_t*)_ref)
+
+Queue::Queue(uint32_t elementSize,uint32_t depth)
+{
+    _ref = Sys::malloc(sizeof(mqd_t));
+    mqd_t* pq=PQ;
+
+     struct mq_attr attr;
+
+    /* initialize the queue attributes */
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = depth;
+    attr.mq_msgsize = elementSize;
+    attr.mq_curmsgs = 0;
+
+    /* create the message queue */
+     mq = mq_open("/MQ", O_CREAT | O_RDWR, 0777, &attr);
+
+    if ( mq < 0 ) perror("mq_open");
+    _msgSize = elementSize;
+}
+
+Queue::~Queue()
+{
+    //dtor
+}
+
+
+
+Erc Queue::put(void* data)
+{
+    if ( mq_send(mq, (const char *)data ,_msgSize,1) < 0 ) perror("mq_send");
+    return E_OK;
+}
