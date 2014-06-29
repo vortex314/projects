cd26da8e9bb913fd4d2d650d0a36af6469e9286c
Lieven Merckx <lieven@pcdebian.telenet.be>
2014-06-29 19:50:14 +0200

    an interim update

--- Debian_Mqtt/Mqtt/main.cpp
+++ Debian_Mqtt/Mqtt/main.cpp
@@ -46,22 +46,42 @@ void Thread::sleep(uint32_t time)
     ::sleep(time/1000);
 }
 
+template <class TL>
+class Publisher
+{private :
+    typedef struct ListenerStruct {
+        struct ListenerStruct *next;
+        TL* listener;
+    } ListenerList;
+public:
+    void addListener(TL* listener)
+    {
+        _listener = listener;
+    }
+
+private:
+    ListenerList _first;
+};
 
 
 //______________________________________________________________________________________
 #include <time.h>
 #define MAX_TIMERS 10
-class Timer: public Stream
+class Timer;
+class TimerListener
+{
+public:
+    virtual void onTimerExpired(Timer* src)=0;
+};
+class Timer: public Publisher<TimerListener>
 {
 public:
 
-    enum TimerEvents
-    {
-        EXPIRED = 1
-    };
+
     Timer();
     ~Timer();
     Timer(Stream* stream);
+//   void addListener(TimerListener* tl);
     Timer(const Timer& ref);
     Timer(uint32_t value, bool reload);
     void interval(uint32_t interval);
@@ -82,6 +102,7 @@ private:
     bool _isAutoReload;
     bool _isActive;
     bool _isExpired;
+    TimerListener* _listener;
 };
 
 
@@ -94,16 +115,21 @@ void Timer::decAll()
     for (i = 0; i < timerCount; i++)
         timers[i]->dec();
 }
+/*
+void Timer::addListener(TimerListener* tl){
+    _listener = tl;
+}
+*/
 
-
-Timer::Timer()  {
-	//register timer
-	timers[timerCount++] = this;
-	_isActive = false;
-	_isAutoReload = false;
-	_reloadValue = 1000;
-	_counterValue = 1000;
-	_isExpired = false;
+Timer::Timer()
+{
+    //register timer
+    timers[timerCount++] = this;
+    _isActive = false;
+    _isAutoReload = false;
+    _reloadValue = 1000;
+    _counterValue = 1000;
+    _isExpired = false;
 }
 
 Timer::Timer(uint32_t value, bool reload)
@@ -155,7 +181,7 @@ void Timer::dec()
     if (_isActive)
         if (--_counterValue == 0)
         {
-            publish(Timer::EXPIRED);
+            _listener->onTimerExpired(this);
             _isExpired = true;
             if (_isAutoReload)
                 _counterValue = _reloadValue;
@@ -232,68 +258,34 @@ public:
         }
     }
 };
-/*
-class MqttThread : public Stream,Thread
-{
-public:
-    MqttThread( const char *name, unsigned short stackDepth, char priority):Thread(name, stackDepth, priority)
-    {
-    };
-    void run()
-    {
-        while(true)
-        {
-            MqttIn mqttIn(256);
-            MqttOut mqttOut(256);
-
-            Str str(100);
-            uint16_t messageId=1000;
-
-            while ( tcp.connect("test.mosquitto.org",1883) != E_OK )
-                sleep(5000);
-            mqttOut.Connect(0, "clientId", MQTT_CLEAN_SESSION,
-                            "ikke/alive", "false", "userName", "password", 1000);
-            tcp.send(&mqttOut);
-            mqttRead();
-
-            Str topic(100),msg(100);
-            topic.append("ikke/topic");
-            msg.append("value");
-            mqttOut.Publish(MQTT_QOS2_FLAG, &str, &msg, messageId);
-
-            tcp.send(&mqttOut);
-            mqttOut.PubRel(messageId);
-            tcp.send(&mqttOut);
-            mqttOut.Disconnect();
-            tcp.send(&mqttOut);
-            tcp.disconnect();
-        }
-    }
-    void mqttRead()
-    {
-        MqttIn mqttIn(256);
-        mqttIn.reset();
-        while ( ! mqttIn.complete() )
-        {
-            mqttIn.add(tcp.read());
-        }
-        mqttIn.parse();
-    }
-
-};
-*/
+//____________________________________________________________________________________________
 #include "CircBuf.h"
 #include "Mqtt.h"
+class Tcp;
 
-class Tcp : public Thread, public Stream
+class TcpListener
+{
+public:
+    virtual void onTcpConnect(Tcp* src)=0;
+    virtual void onTcpDisconnect(Tcp* src)=0;
+    virtual void onTcpMessage(Tcp* src,Bytes* data)=0;
+};
+
+class Tcp : public Thread
 {
 private:
     int _sockfd;
+    TcpListener *_listener;
 public:
     enum { TCP_CONNECTED, TCP_DISCONNECTED, TCP_PACKET } TcpEvents;
     Tcp( const char *name, unsigned short stackDepth, char priority):Thread(name, stackDepth, priority)
     {
     };
+
+    void addListener(TcpListener *listener)
+    {
+        _listener=listener;
+    }
 
     Erc connect(char *ip,int portno)
     {
@@ -377,7 +369,7 @@ public:
         {
             while ( connect((char*)"localhost",1883) != E_OK )
                 sleep(5000);
-            publish(TCP_CONNECTED);
+            _listener->onTcpConnect(this);
 
             while(true)
             {
@@ -385,7 +377,7 @@ public:
                 if ( b < 0 ) break;
                 mqttRead(b);
             }
-            publish(TCP_DISCONNECTED);
+            _listener->onTcpDisconnect(this);
             disconnect();
             sleep(5000);
         }
@@ -398,7 +390,7 @@ public:
         if (  mqttIn->complete() )
         {
             mqttIn->parse();
-            publish(TCP_PACKET,mqttIn);
+            _listener->onTcpMessage(this,mqttIn);
             mqttIn=new MqttIn(256);
             mqttIn->reset();
         }
@@ -409,70 +401,97 @@ private:
     CircBuf rxdBuffer;
 };
 
-class MqttConnector : public Stream
+class MqttListener
+{
+    virtual void onMqttMessage(MqttIn* msg)=0;
+    virtual void onMqttConnect()=0;
+    virtual void onMqttDisconnect()=0;
+};
+
+
+class Mqtt : public TcpListener,public TimerListener,public Publisher<MqttListener>
+
 {
 private:
 
     Tcp* _tcp;
     MqttOut* _mqttOut;
+    Timer* _timer;
 
 public:
     enum { MQTT_CONNECTED=100, MQTT_DISCONNECTED } Events;
 
-    MqttConnector( )
-    {
-    };
-    void init(Tcp* tcp)
+    Mqtt( Tcp* tcp)
     {
         _tcp=tcp;
         _tcp->addListener(this);
+        _timer =  new Timer();
+        _timer->addListener(this);
         _mqttOut = new MqttOut(256);
     }
-    void eventHandler(Event* event)
+    void onTcpConnect(Tcp* src)
     {
-        if ( event->is(_tcp,Tcp::TCP_CONNECTED))
-        {
-            _mqttOut->Connect(0, "clientId", MQTT_CLEAN_SESSION,
-                              "ikke/alive", "false", "", "", 1000);
-            _tcp->send(_mqttOut);
-        }
-        else if ( event->is(_tcp,Tcp::TCP_DISCONNECTED))
-        {
-            publish(MQTT_DISCONNECTED);
+        _mqttOut->Connect(0, "clientId", MQTT_CLEAN_SESSION,
+                          "ikke/alive", "false", "", "", 1000);
+        _tcp->send(_mqttOut);
+    }
+    void onTcpDisconnect(Tcp* src)
+    {
 
-        }
-        else if ( event->is(_tcp,Tcp::TCP_PACKET))
+    }
+    void onTcpMessage(Tcp* src,Bytes* data)
+    {
+        MqttIn *packet=(MqttIn*)data;
+        if ( packet->type() == MQTT_MSG_CONNACK)
         {
-            MqttIn *packet=(MqttIn*)event->data();
-            if ( packet->type() == MQTT_MSG_CONNACK)
+            if ( packet->_returnCode == MQTT_RTC_CONN_ACC)
             {
-                if ( packet->_returnCode == MQTT_RTC_CONN_ACC)
-                    publish(MQTT_CONNECTED);
-                else
-                {
-                    std::cerr << "Mqtt Connect failed , return code : " << packet->_returnCode;
-                    _tcp->disconnect();
-                }
+            }
+//                   publish(MQTT_CONNECTED);
+            else
+            {
+                std::cerr << "Mqtt Connect failed , return code : " << packet->_returnCode;
+                _tcp->disconnect();
             }
         }
-        else
-        {
-            std::cerr << "unexpected event : "<< event->id()<< std::endl;
-        }
+    };
+    void onTimerExpired(Timer* timer)
+    {
     }
+    /*   void eventHandler(Event* event)
+       {
+           if ( event->is(_tcp,Tcp::TCP_CONNECTED))
+           {
+               onConnect(_tcp);
+           }
+           else if ( event->is(_tcp,Tcp::TCP_DISCONNECTED))
+           {
+               onDisconnect(_tcp);
+
+           }
+           else if ( event->is(_tcp,Tcp::TCP_PACKET))
+           {
+               onMessage(_tcp,(MqttIn*)event->data());
+           }
+           else
+           {
+               std::cerr << "unexpected event : "<< event->id()<< std::endl;
+           }
+       }*/
 };
+
 #include "Property.h"
 #include "Strpack.h"
 uint32_t p=1234;
 Property property(&p, T_INT32, M_READ, "ikke/P","$META");
 
 //_____________________________________________________________________________________________________
-class MqttPublisher : public Stream
+class MqttPublisher : public TimerListener,public MqttListener
 {
 private:
 
     Tcp* _tcp;
-    MqttConnector* _mqttConnector;
+    Mqtt* _mqtt;
     bool _isConnected;
     Timer* _timer;
     Property* _currentProperty;
@@ -490,61 +509,75 @@ public:
         _retryCount=0;
 
     };
-    void init(Tcp* tcp,MqttConnector* mqttConnector)
+    void init(Mqtt* mqtt)
     {
-        _mqttConnector = mqttConnector;
-        _mqttConnector->addListener(this);
-        _tcp=tcp;
-        _tcp->addListener(this);
+        _mqtt = mqtt;
+        _mqtt->addListener(this);
+//        _tcp=tcp;
+//       _tcp->addListener(this);
         _timer->addListener(this);
 
     }
-    void eventHandler(Event* event)
+    void onTimerExpired(Timer* timer)
     {
-        if ( event->is(_timer,Timer::EXPIRED))
+    }
+    void onMqttMessage(MqttIn* msg)
+    {
+        if ( msg->type() == MQTT_MSG_PUBACK)
         {
-            if ( _state == ST_WAIT_ACK )
-            {
-                if ( _retryCount++< 3 )
-                    publishCurrentProperty();
-            }
-        }
-        else if ( event->is(_mqttConnector,MqttConnector::MQTT_CONNECTED))
-        {
-            _isConnected = true;
-
-            _state = ST_WAIT_ACK;
-            _retryCount=0;
-        }
-        else if ( event->is(_mqttConnector,MqttConnector::MQTT_DISCONNECTED))
-        {
-            _isConnected = false;
+            _timer->stop();
             _state = ST_SLEEP;
 
         }
-        else if ( event->is(_tcp,Tcp::TCP_PACKET))
+        else if ( msg->type() == MQTT_MSG_PUBREC)
         {
-            MqttIn *packet=(MqttIn*)event->data();
-            if ( packet->type() == MQTT_MSG_PUBACK)
-            {
-                _timer->stop();
-                _state = ST_SLEEP;
-
-            }
-            else if ( packet->type() == MQTT_MSG_PUBREC)
-            {
-
-            }
-            else if ( packet->type() == MQTT_MSG_PUBCOMP)
-            {
-
-            }
 
         }
-        else
+        else if ( msg->type() == MQTT_MSG_PUBCOMP)
         {
-            std::cerr << "unexpected event : "<< event->id()<< std::endl;
+
         }
+    }
+    void onMqttConnect()
+    {
+        _isConnected = true;
+
+        _state = ST_WAIT_ACK;
+        _retryCount=0;
+    };
+    void onMqttDisconnect()
+    {
+        _isConnected = false;
+        _state = ST_SLEEP;
+    };
+    void eventHandler(Event* event)
+    {
+        /*      if ( event->is(_timer,Timer::EXPIRED))
+              {
+                  if ( _state == ST_WAIT_ACK )
+                  {
+                      if ( _retryCount++< 3 )
+                          publishCurrentProperty();
+                  }
+              }
+              else if ( event->is(_mqttConnector,MqttConnector::MQTT_CONNECTED))
+              {
+
+              }
+              else if ( event->is(_mqttConnector,MqttConnector::MQTT_DISCONNECTED))
+              {
+
+
+              }
+            else if ( event->is(_tcp,Tcp::TCP_PACKET))
+              {
+
+
+              }
+              else
+              {
+                  std::cerr << "unexpected event : "<< event->id()<< std::endl;
+              }*/
     }
 
     void publishCurrentProperty()
@@ -560,12 +593,12 @@ public:
     }
 };
 //_____________________________________________________________________________________________________
-class MqttSubscriber : public Stream
+class MqttSubscriber : public MqttListener
 {
 private:
 
     Tcp* _tcp;
-    MqttConnector* _mqttConnector;
+    Mqtt* _mqtt;
     bool _isConnected;
     uint16_t _messageId;
     MqttOut* _mqttOut;
@@ -582,77 +615,75 @@ public:
         _tempTopic = new Str(30);
         _tempMessage = new Str(100);
     };
-    void init(Tcp* tcp,MqttConnector* mqttConnector)
+    void init(Mqtt* mqtt)
     {
-        _mqttConnector = mqttConnector;
-        _mqttConnector->addListener(this);
-        _tcp=tcp;
-        _tcp->addListener(this);
+        _mqtt = mqtt;
+        _mqtt->addListener(this);
+//        _tcp=tcp;
+//       _tcp->addListener(this);
     }
-    void eventHandler(Event* event)
+
+    void onTimerExpired(Timer* timer)
     {
-        if ( event->is(_mqttConnector,MqttConnector::MQTT_CONNECTED))
-        {
-            _state = SUBSCRIBING;
-            _isConnected = true;
-            MqttOut out(256);
-            out.Subscribe(0, "ikke/#", ++ _messageId, MQTT_QOS1_FLAG);
-            _tcp->send(&out);
-        }
-        else if ( event->is(_mqttConnector,MqttConnector::MQTT_DISCONNECTED))
-        {
-            _isConnected = false;
-            _state = DISCONNECTED;
 
-        }
-        else if ( event->is(_tcp,Tcp::TCP_PACKET))
-        {
-            MqttIn *packet=(MqttIn*)event->data();
-            if ( packet->type() == MQTT_MSG_SUBACK)
-            {
-                _state=SUBSCRIBED;
-            }
-            else if ( packet->type() == MQTT_MSG_PUBLISH)
-            {
-                Str topic(100);
-                Str message(100);
-                topic.write(&packet->_topic);
-                message.write(&packet->_message);
-                std::cout << topic.data() << ":" << message.data() << " QOS " << (packet->_header & MQTT_QOS_MASK )<< std::endl;
-                if (( packet->_header & MQTT_QOS_MASK )== MQTT_QOS1_FLAG )
-                {
-                    _mqttOut->PubAck(packet->messageId());
-                    _tcp->send(_mqttOut);
-                    // execute setter
-                }
-                else if (( packet->_header & MQTT_QOS_MASK )== MQTT_QOS2_FLAG )
-                {
-                    _mqttOut->PubRec(packet->messageId());
-                    _tcp->send(_mqttOut);
-                    _tempTopic->clear();
-                    _tempMessage->clear();
-                    _tempTopic->write(&packet->_topic);
-                    _tempMessage->write(&packet->_message);
-                }
-                else
-                {
-                    // execute setter
-                }
+    }
 
-            }
-            else if ( packet->type() == MQTT_MSG_PUBREL)
+    void onMqttMessage(MqttIn* msg)
+    {
+        if ( msg->type() == MQTT_MSG_SUBACK)
+        {
+            _state=SUBSCRIBED;
+        }
+        else if ( msg->type() == MQTT_MSG_PUBLISH)
+        {
+            Str topic(100);
+            Str message(100);
+            topic.write(&msg->_topic);
+            message.write(&msg->_message);
+            std::cout << topic.data() << ":" << message.data() << " QOS " << (msg->_header & MQTT_QOS_MASK )<< std::endl;
+            if (( msg->_header & MQTT_QOS_MASK )== MQTT_QOS1_FLAG )
             {
-                // execute setter with _temp
-                std::cout << " PUBREL " << _tempTopic->data() << ":" << _tempMessage->data() << std::endl;
-                _mqttOut->PubComp(packet->messageId());
+                _mqttOut->PubAck(msg->messageId());
                 _tcp->send(_mqttOut);
+                // execute setter
             }
+            else if (( msg->_header & MQTT_QOS_MASK )== MQTT_QOS2_FLAG )
+            {
+                _mqttOut->PubRec(msg->messageId());
+                _tcp->send(_mqttOut);
+                _tempTopic->clear();
+                _tempMessage->clear();
+                _tempTopic->write(&msg->_topic);
+                _tempMessage->write(&msg->_message);
+            }
+            else
+            {
+                // execute setter
+            }
+
         }
-        else
+        else if ( msg->type() == MQTT_MSG_PUBREL)
         {
-            std::cerr << "unexpected event : "<< event->id()<< std::endl;
+            // execute setter with _temp
+            std::cout << " PUBREL " << _tempTopic->data() << ":" << _tempMessage->data() << std::endl;
+            _mqttOut->PubComp(msg->messageId());
+            _tcp->send(_mqttOut);
         }
     }
+    void onMqttConnect()
+    {
+        _state = SUBSCRIBING;
+        _isConnected = true;
+        MqttOut out(256);
+        out.Subscribe(0, "ikke/#", ++ _messageId, MQTT_QOS1_FLAG);
+        _tcp->send(&out);
+    };
+    void onMqttDisconnect()
+    {
+        _isConnected = false;
+        _state = DISCONNECTED;
+    };
+
 };
 
 
@@ -660,12 +691,12 @@ int main(int argc, char *argv[])
 {
 //    MqttThread mqtt("mqtt",1000,1);
     Tcp tcp("tcp",1000,1);
-    MqttConnector mqttConnector;
-    mqttConnector.init(&tcp);
+    Mqtt mqtt(&tcp);
     MqttPublisher mqttPublisher;
-    mqttPublisher.init(&tcp,&mqttConnector);
+
+    mqttPublisher.init(&mqtt);
     MqttSubscriber mqttSubscriber;
-    mqttSubscriber.init(&tcp,&mqttConnector);
+    mqttSubscriber.init(&mqtt);
     TimerThread tt("TimerThread",1000,1);
     MainThread mt("messagePump",1000,1);
 
