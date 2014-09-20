08ba17231b283a3a0129aa3a34d5e489548e5c86
Lieven Merckx <lieven.merckx@gmail.com>
2014-09-20 09:18:34 +0200

    system kept on misaligning static data for initialization, .map file showed extra's and had to adapt linker scriprt to take into account other sections

--- StellarisUsb/src/malloc.c
+++ StellarisUsb/src/malloc.c
@@ -132,7 +132,7 @@ register void * stack_pointer asm ("r15");
 
 #ifdef DEFINE_MALLOC
 
- uint64_t filler=0xBAD0BEAF;
+ uint64_t m_count_called=0;
  void * __malloc_end = (void*)(&__malloc_start);
 
 // void * __malloc_end = &_end;
@@ -147,7 +147,7 @@ malloc (size_t sz)
   /* real_size is the size we actually have to allocate, allowing for
      overhead and alignment.  */
   size_t real_size = REAL_SIZE (sz);
-  filler++;
+  m_count_called++;
 
   /* Look for the first block on the freelist that is large enough.  */
   for (nextfree = &__malloc_freelist;
@@ -227,6 +227,7 @@ void
 free (void *block_p)
 {
   fle *nextfree;
+  m_count_called--;
   fle block = (fle)((size_t) block_p - offsetof (struct freelist_entry, next));
 
   if (block_p == NULL)
