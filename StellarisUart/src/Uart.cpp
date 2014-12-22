211cb68b5fa769bb9928d52dd7cd52185151ca10
vortex314 <lieven.merckx@gmail.com>
2014-12-22 22:28:58 +0100

    update

--- StellarisUart/src/Uart.cpp
+++ StellarisUart/src/Uart.cpp
@@ -40,7 +40,7 @@ void Uart::init() {
 	GPIOPinConfigure(GPIO_PA0_U0RX);
 	GPIOPinConfigure(GPIO_PA1_U0TX);
 	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-	UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+	UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 1000000,
 			(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
 			UART_CONFIG_PAR_NONE));
 	// Enable the UART interrupt.
