bbe573cb7f14cecd70b750165e88f1bd63ae0d10
vortex314 <lieven.merckx@gmail.com>
2015-02-09 22:31:14 +0100

    updating flash routines

--- StellarisUart/src/Persistent.cpp
+++ StellarisUart/src/Persistent.cpp
@@ -16,9 +16,17 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/flash.h"
 #include "driverlib/debug.h"
+/*
 
+ PageHeader : <sequence><FFFFFFFF>
+ highets sequence is active page
+ 4 x 1 Kb page
+ <00><FF>
+ <lengthRound4><realLength><id> <data4>
+ < 0xFF>< 8 bit >< 8 bit >< 8 bit >
+
+ */
 // <max_length(4)><real_length(4)><name><max_length><length><data>
-
 #define ROUNDUP4(xxx) ((xxx + 3 ) & 0xFFFC)
 
 typedef struct {
@@ -27,6 +35,126 @@ typedef struct {
 	uint8_t used;
 	uint8_t tag;
 } Header;
+
+void Persistent::pageInit(uint8_t* address, uint32_t sequence) {
+	FlashErase((uint32_t) address);
+	FlashProgram(&sequence, (uint32_t) address, 4);
+}
+
+void Persistent::pageErase(uint8_t* address) {
+	FlashErase((uint32_t) address);
+}
+
+bool Persistent::pageFindTag(uint8_t* address, uint8_t tag, uint8_t** addr) {
+	uint8_t* ptr = address + 8;
+	uint8_t* tagAddress = 0;
+	while (ptr < address + 0x400) {
+		Header* pHdr = (Header*) ptr;
+		if (*(uint32_t*) ptr == 0xFFFFFFFF) {
+			if (tagAddress == 0) {
+				*addr = ptr;
+				return false;;
+			} else {
+				*addr = tagAddress;
+				return true;
+			}
+		}
+		if (pHdr->tag == tag) {
+			tagAddress = ptr;
+		}
+		ptr += pHdr->allocated + 4;
+		if (pHdr->allocated == 0)
+			break;
+	}
+	return false;
+}
+
+bool Persistent::pageGetTag(uint8_t* page, uint8_t tag, uint8_t** start,
+		uint8_t* length) {
+	uint8_t* addr = 0;
+
+	if (!pageFindTag(page, tag, &addr))
+		return false;
+
+	Header* pHdr = (Header*) addr;
+	if (pHdr->used > length)
+		return false;
+
+	memcpy(*start, (uint8_t*) addr + 4, pHdr->used);
+	length = pHdr->used;
+	return true;
+}
+
+uint32_t pageSpaceLeft(uint8_t* page) {
+	uint8_t* addr = pageFreeBegin(page);
+	return page + 0x400 - addr;
+}
+
+uint32_t pageSequence(uint8_t* page) {
+	return *(uint32_t*) page;
+}
+
+bool pageIsUsed(uint8_t* page) {
+	return (*(uint32_t*) page != 0xFFFFFFFF
+			&& *(uint32_t*) (page + 4) == 0xFFFFFFFF);
+}
+
+uint8_t* pageFreeBegin(uint8_t* page) {
+	uint8_t* addr;
+	uint8_t* ptr = page + 8;
+	while (ptr < page + 0x400) {
+		Header* pHdr = (Header*) ptr;
+		if (*(uint32_t*) ptr == 0xFFFFFFFF) {
+			*addr = ptr;
+			return addr;
+		}
+		ptr += pHdr->allocated;
+		if (pHdr->allocated == 0)
+			break;
+	}
+	return 0;
+}
+
+void Persistent::findActivePage() {
+	uint8_t* page;
+	uint32_t* activePage = 0;
+	uint32_t sequence;
+	for (page = _flashStart; page < _flashEnd; page += 0x100) {
+		if (pageIsUsed(page))
+			if (*(uint32_t*) page != 0xFFFFFFFF
+					&& *(uint32_t*) (page + 4) == 0xFFFFFFFF)
+				if (*page > sequence) {
+					activePage = page;
+					sequence = *page;
+				}
+	}
+	if (activePage) {
+		_pageActive = activePage;
+		_sequence = sequence;
+	} else {
+		_pageActive = _flashStart;
+		_sequence = 0;
+		pageInit(_pageActive, _sequence);
+	}
+
+}
+
+uint8_t* Persistent::pageNext(uint8_t * activePage) {
+	uint8_t* page = activePage + 0x400;
+	if (page >= _flashEnd)
+		page = _flashStart;
+}
+
+void Persistent::pageCopy(uint8_t* dst, uint8_t* src) {
+	uint8_t* page = pageNext(src);
+	uint8_t* tagAddress;
+	uint8_t length;
+	pageInit(dst, _sequence + 1);
+	for (uint8_t tag = 0; tag < 10; tag++) {
+		if (pageFindTag(src, tag, tagAddress))
+			pagePutTag(page, tag);
+	}
+}
 
 bool Persistent::isPageActive(uint8_t* ptr) {
 	return (*ptr == 0 && *(ptr + 1) == 0xFFFFFFFF);
@@ -39,21 +167,19 @@ bool Persistent::setPageActive(uint8_t* ptr) {
 	return true;
 }
 
-bool Persistent::findPageActive(){
+bool Persistent::findPageActive() {
 	uint8_t* ptr;
-		for (ptr = _flashStart; ptr < _flashEnd; ptr += 0x400) {
-			if (isPageActive(ptr)) {
-				_pageActive = ptr;
-				break;
-			}
+	for (ptr = _flashStart; ptr < _flashEnd; ptr += 0x400) {
+		if (isPageActive(ptr)) {
+			_pageActive = ptr;
+			break;
 		}
-		if (ptr == _flashEnd) {
-			setPageActive(_flashStart);
-		}
-		return true;
+	}
+	if (ptr == _flashEnd) {
+		setPageActive(_flashStart);
+	}
+	return true;
 }
-
-
 
 Persistent::Persistent() {
 	_flashStart = (uint8_t*) 0x30000; // after 3 x 64KB see linker script
@@ -68,9 +194,13 @@ Persistent::~Persistent() {
 bool Persistent::put(uint8_t index, uint8_t* data, uint8_t length) {
 // find address
 	uint8_t* address = 0;
-	if ( !freeSpace(&address)) return false;
+	if (!freeSpace(&address))
+		return false;
+	if (address + 4 + ROUNDUP4(length) >= _activePage + 0x400) {
+		switchPage(&address);
+	}
 	Header hdr = { 0xFF, ROUNDUP4(length), length, index };
-	if (FlashProgram((uint32_t*) &hdr, (uint32_t)address, 4))
+	if (FlashProgram((uint32_t*) &hdr, (uint32_t) address, 4))
 		return false;
 	union {
 		uint32_t i32;
@@ -83,7 +213,7 @@ bool Persistent::put(uint8_t index, uint8_t* data, uint8_t length) {
 		} else
 			l = length - i;
 		memcpy(v.b, data + i, l);
-		FlashProgram(&v.i32, (uint32_t)(address + 4 + i), 4);
+		FlashProgram(&v.i32, (uint32_t) (address + 4 + i), 4);
 	}
 	return true;
 }
@@ -91,7 +221,7 @@ bool Persistent::put(uint8_t index, uint8_t* data, uint8_t length) {
 bool Persistent::get(uint8_t tag, uint8_t* data, uint8_t& length) {
 // find address
 	uint8_t* address = 0;
-	if (!findTag(tag, &address))
+	if (!pageFindTag(_pageActive,tag, &address))
 		return false;
 
 	Header* pHdr = (Header*) address;
@@ -102,17 +232,14 @@ bool Persistent::get(uint8_t tag, uint8_t* data, uint8_t& length) {
 	return true;
 }
 
-
-
-
 bool Persistent::findTag(uint8_t tag, uint8_t** addr) {
 	uint8_t* ptr = _pageActive + 8;
 	uint8_t* tagAddress = 0;
 	while (ptr < _pageActive + 0x400) {
 		Header* pHdr = (Header*) ptr;
-		if (*(uint32_t*)ptr == 0xFFFFFFFF) {
+		if (*(uint32_t*) ptr == 0xFFFFFFFF) {
 			if (tagAddress == 0) {
-				*addr =  ptr;
+				*addr = ptr;
 				return false;;
 			} else {
 				*addr = tagAddress;
@@ -122,23 +249,22 @@ bool Persistent::findTag(uint8_t tag, uint8_t** addr) {
 		if (pHdr->tag == tag) {
 			tagAddress = ptr;
 		}
-		ptr += pHdr->allocated +4 ;
+		ptr += pHdr->allocated + 4;
 		if (pHdr->allocated == 0)
 			break;
 	}
 	return false;
 }
 
-
 bool Persistent::freeSpace(uint8_t** addr) {
 	uint8_t* ptr = _pageActive + 8;
 	while (ptr < _pageActive + 0x400) {
 		Header* pHdr = (Header*) ptr;
-		if (*(uint32_t*)ptr == 0xFFFFFFFF) {
-			*addr =  ptr;
+		if (*(uint32_t*) ptr == 0xFFFFFFFF) {
+			*addr = ptr;
 			return true;
 		}
-		ptr += pHdr->allocated ;
+		ptr += pHdr->allocated;
 		if (pHdr->allocated == 0)
 			break;
 	}
@@ -148,5 +274,3 @@ bool Persistent::freeSpace(uint8_t** addr) {
 bool Persistent::erasePage(uint8_t* ptr) {
 	return FlashErase((uint32_t) ptr) == 0;
 }
-
-
--- StellarisUart/src/Persistent.h
+++ StellarisUart/src/Persistent.h
@@ -9,15 +9,16 @@
 #define PERSISTENT_H_
 #include <stdint.h>
 
-	enum PERS_ID {
-		PERS_0,PERS_MQTT_PREFIX, PERS_MQTT_HOST, PERS_MQTT_PORT
-	} ;
+enum PERS_ID {
+	PERS_0, PERS_MQTT_PREFIX, PERS_MQTT_HOST, PERS_MQTT_PORT
+};
 
 class Persistent {
 private:
 	uint8_t* _flashStart;
 	uint8_t* _flashEnd;
 	uint8_t* _pageActive;
+	uint32_t _sequence;
 public:
 
 	Persistent();
@@ -29,9 +30,19 @@ public:
 	bool isPageActive(uint8_t* ptr);
 	bool setPageActive(uint8_t* ptr);
 	bool findPageActive();
+	void findActivePage();
 	bool erasePage(uint8_t* ptr);
 	bool findTag(uint8_t tag, uint8_t** offset);
-	bool freeSpace(uint8_t** addr) ;
+	bool freeSpace(uint8_t** addr);
+
+	void pageInit(uint8_t* page, uint32_t sequence);
+	void pageErase(uint8_t* page);
+	bool pageFindTag(uint8_t* page, uint8_t tag, uint8_t** address);
+	bool pageGetTag(uint8_t* page, uint8_t index, uint8_t** data,
+			uint8_t* length);
+	uint8_t* pageNext(uint8_t* page);
+	void pageCopy(uint8_t* dst, uint8_t* src);
+	uint8_t* pageFreeBegin(uint8_t* page);
 };
 
 #endif /* PERSISTENT_H_ */
