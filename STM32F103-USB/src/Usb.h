013ab53e849af353534122c4ebb528ff0ccd4e81
vortex314 <lieven.merckx@gmail.com>
2015-07-10 01:18:43 +0200

    syncing

---
+++ STM32F103-USB/src/Usb.h
@@ -0,0 +1,52 @@
+
+/*
+ * Usb.h
+ *
+ *  Created on: 5-jul.-2015
+ *      Author: lieven2
+ */
+
+#ifndef SRC_USB_H_
+#define SRC_USB_H_
+#include <stdint.h>
+
+#ifdef __cplusplus
+#include <CircBuf.h>
+#include <Handler.h>
+#include <Link.h>
+#include <MqttIn.h>
+class Usb : public Link {
+public:
+	Usb();
+	virtual ~Usb();
+	bool _isConnected;
+	MqttIn _mqttIn;
+	CircBuf _out;
+	CircBuf _in;
+	Handler* _device;
+	static void init();
+	void reset();
+	Erc connect();
+	Erc disconnect();
+	Erc send(Bytes& bytes);
+	Erc recv(Bytes& bytes);
+	uint8_t read();
+	uint32_t hasData();
+	bool dispatch(Msg& event) ;
+};
+#endif
+// calleable from C code and interrupt routines
+#ifdef __cplusplus
+extern "C" {
+#endif
+	void onUsbConnected();
+	void onUsbDisconnected();
+	void onUsbRxData(uint8_t* pData,uint32_t length);
+	uint32_t usbTxDataSize();
+	uint32_t usbGetTxData(uint8_t* pData,uint32_t length);
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* SRC_USB_H_ */
---
+++ STM32F103-USB/src/main.cpp
@@ -0,0 +1,106 @@
+
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  MCD Application Team
+ * @version V4.0.0
+ * @date    21-January-2013
+ * @brief   Virtual Com Port Demo main file
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+ *
+ * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *        http://www.st.com/software_license_agreement_liberty_v2
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+//#include "hw_config.h"
+// #include "usb_lib.h"
+// #include "usb_desc.h"
+// #include "usb_pwr.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Extern variables ----------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/*******************************************************************************
+ * Function Name  : main.
+ * Description    : Main routine.
+ * Input          : None.
+ * Output         : None.
+ * Return         : None.
+ *******************************************************************************/
+extern "C" void initBoard();
+
+#include "Timer.h"
+#include "Msg.h"
+#include "Handler.h"
+#include "Usb.h"
+#include "Mqtt.h"
+
+extern Usb usb;
+
+int main(void) {
+	initBoard();
+
+	Mqtt mqtt(usb);
+
+	uint64_t clock = Sys::upTime() + 100;
+	MsgQueue::publish(0, SIG_INIT, 0, 0);				// kickoff all engines
+	Msg msg;
+
+	while (1) {
+		if (Sys::upTime() > clock) {
+			clock += 10;		// 10 msec timer tick
+			MsgQueue::publish(0, SIG_TICK, 0, 0); // check timeouts every 10 msec
+		}
+		if (usb.hasData())	// if UART has received data alert uart receiver
+			MsgQueue::publish(&usb, SIG_START);
+		// _________________________________________________________________handle all queued messages
+		while (MsgQueue::get(msg)) {
+			Handler::dispatchToChilds(msg);
+
+		}
+
+	}
+}
+#ifdef USE_FULL_ASSERT
+/*******************************************************************************
+ * Function Name  : assert_failed
+ * Description    : Reports the name of the source file and the source line number
+ *                  where the assert_param error has occurred.
+ * Input          : - file: pointer to the source file name
+ *                  - line: assert_param error line source number
+ * Output         : None
+ * Return         : None
+ *******************************************************************************/
+void assert_failed(uint8_t* file, uint32_t line)
+{
+	/* User can add his own implementation to report the file name and line number,
+	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+	/* Infinite loop */
+	while (1)
+	{}
+}
+#endif
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
